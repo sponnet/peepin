29a5057399b657f71be8bb4c7eda41ead2a99d19
Stefaan Ponnet <stefaan@ponnet.com>
2018-08-29 03:01:23 +0200

    reconnect web3 from time to time to avoid errors

--- src/web3.js
+++ src/web3.js
@@ -1,9 +1,20 @@
-const Web3 = require('web3');
+const Web3 = require("web3");
 let globalweb3;
 
-module.exports = (socketUrl) => {
-	if (!globalweb3) {
-		globalweb3 = new Web3(new Web3.providers.WebsocketProvider(socketUrl));
-	}
-	return globalweb3;
+module.exports = socketUrl => {
+  if (!globalweb3) {
+    globalweb3 = new Web3(new Web3.providers.WebsocketProvider(socketUrl));
+  }
+
+  // check connectivity to Web3 socket, reconnect if neccesary
+  setInterval(() => {
+    globalweb3.eth.net
+      .isListening()
+      .then()
+      .catch(e => {
+        globalweb3.setProvider(new Web3.providers.WebsocketProvider(socketUrl));
+      });
+  }, 10 * 1000);
+
+  return globalweb3;
 };
